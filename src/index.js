70b592cfe1398c2326063ca9b193eb996d807d59
ghd6840@naver.com <ghd6840@naver.com>
2024-02-01 12:39:11 +0000

    State

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
-import CommentList from './chapter_05/CommentList';
+//import CommentList from './chapter_05/CommentList';
 // import Clock from './chapter_04/Clock';
+import NotificationList from './chapter_06/NotificationList';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
   <React.StrictMode>
-    <CommentList />
+    <NotificationList />
   </React.StrictMode>,
   document.getElementById('root')
 );
