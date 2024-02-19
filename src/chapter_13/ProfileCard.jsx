c84447d3687cde3e55f30a711841434c982517ee
ghd6840@naver.com <ghd6840@naver.com>
2024-02-19 12:32:23 +0000

    Composition(Specialization,Containment)

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+    return(
+        <Card title="Kyle Hong" backgroundColor="#ff0000">
+            <p>안녕하세요, 카일입니다.</p>
+            <p>리액트 기본개념 다시 한번 복습중입니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
